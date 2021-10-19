e9d44d9aaf14d7fd57b830b40b797adb3b81d6f9
MNewbigging <mrnewbigging@gmail.com>
2021-10-19 20:41:21 +0100

    updated gitignore and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: 'file-loader?name=./assets/[name].[ext]',
+        use: 'file-loader?name=[name].[ext]&outputPath=./assets/',
       },
       {
         test: /\.svg$/,
