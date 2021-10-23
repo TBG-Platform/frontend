a8a62fe31e4f43a16e41889de14640011531e206
MNewbigging <mrnewbigging@gmail.com>
2021-10-23 16:23:15 +0100

    Installed blueprintjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@blueprintjs/core';
 import { observer } from 'mobx-react';
 import React from 'react';
 import { AppState } from './AppState';
@@ -6,6 +7,12 @@ import { AppState } from './AppState';
 export class App extends React.PureComponent {
   private readonly appState = new AppState();
   public render() {
-    return <button onClick={() => this.appState.incCount()}>Clicks: {this.appState.count}</button>;
+    return (
+      <Button
+        icon={'add'}
+        text={`Clicks: ${this.appState.count}`}
+        onClick={() => this.appState.incCount()}
+      />
+    );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,26 @@ module.exports = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
       },
+      {
+        test: /\.(ttf|eot|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[hash].[ext]',
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: 'fonts/[hash].[ext]',
+            limit: 5000,
+            mimetype: 'application/font-woff',
+          },
+        },
+      },
     ],
   },
 
