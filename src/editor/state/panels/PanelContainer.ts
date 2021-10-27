b7b39702faee4f1e1b260f45406160c63a257709
MNewbigging <mrnewbigging@gmail.com>
2021-10-27 22:33:57 +0100

    notes

--- src/editor/state/panels/PanelContainer.ts
+++ src/editor/state/panels/PanelContainer.ts
@@ -41,3 +41,82 @@ export class PanelContainer {
     }
   }
 }
+
+/**
+ * NOTES
+ *
+ * The data model is starting to get really messy to use and make sense of.
+ * Needs a simpler approach.
+ *
+ * Observations:
+ *
+ * Parent: ROW
+ *  - Panel
+ *
+ * If I split the panel right, this adds a child to the parent
+ *
+ * Parent: ROW
+ *  - Panel
+ *  - Panel
+ *
+ * They share basis percentage of the parent
+ *
+ * If I split the first panel right again, it should:
+ *
+ * Parent: ROW
+ *  - Panel
+ *  - Panel
+ *  - Panel
+ *
+ * But it actually does:
+ *
+ * Parent: ROW
+ *  - Parent: ROW
+ *     - Panel
+ *     - Panel
+ *  - Panel
+ *
+ * So if I split in a way that follows the flow, it adds a sibling panel?
+ *
+ * Back to:
+ *
+ * Parent: ROW
+ *  - Panel
+ *  - Panel
+ *
+ * If I split the second bottom:
+ *
+ * Parent: ROW
+ *  - Panel
+ *  - Parent: COL
+ *     - Panel
+ *     - Panel
+ *
+ * And split the last bottom it should:
+ *
+ * Parent: ROW
+ *  - Panel
+ *  - Parent: COL
+ *     - Panel
+ *     - Panel
+ *     - Panel
+ *
+ * Ok, so a parent can have any number of panel children that follow the parent's flow.
+ * As soon as I split contrary to the parent's flow, it creates a new parent.
+ *
+ * This may at least remove the 'a' and 'b' props/logic, which is suepr messy...
+ * However, having an array of child items also increases the complexity of resizing,
+ * because it would need to take into account all siblings, not just work between two siblings.
+ *
+ * As for the either/or type logic:
+ *
+ * Parent: ROW
+ *  - Panel
+ *  - Parent: COL
+ *     - Panel
+ *     - Panel
+ *  - Panel
+ *  - Panel
+ *
+ * I may have parents amongst panels, so I will still have render-this-or-that logic...
+ */
