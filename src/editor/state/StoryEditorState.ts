c9e093479d1ae1a1a4c4d0fd419cf3142d34df99
MNewbigging <mrnewbigging@gmail.com>
2021-11-02 20:32:56 +0000

    removed details panel state

--- src/editor/state/StoryEditorState.ts
+++ src/editor/state/StoryEditorState.ts
@@ -1,15 +1,14 @@
 import { action, observable } from 'mobx';
-import { keyboardObserver } from '../../utils/KeyboardObserver';
-import { Vector } from '../../utils/Vector';
-import { DetailsPanelFocus, DetailsPanelState } from './DetailsPanelState';
+
 import { Page } from './Page';
 import { Story } from './Story';
 import { StoryGraphState } from '../story-graph/state/StoryGraphState';
+import { Vector } from '../../utils/Vector';
+import { keyboardObserver } from '../../utils/KeyboardObserver';
 
 export class StoryEditorState {
   @observable.ref public story?: Story;
   @observable public addingTextBlock = false;
-  public detailsPanelState = new DetailsPanelState();
   public storyGraphState = new StoryGraphState();
 
   @action public createNewStory = () => {
@@ -51,14 +50,12 @@ export class StoryEditorState {
     // const pageRect = pageDisplayUtil.getPageDisplayBounds();
     // const pagePos = new Vector(pageRect.left, pageRect.top);
     // mousePos.sub(pagePos);
-
     // Position as percentage value
     // const leftPercent = (mousePos.x / pageRect.width) * 100;
     // const topPercent = (mousePos.y / pageRect.height) * 100;
-
     // const pos = new Vector(leftPercent, topPercent);
     //  this.story.selectedPage.addTextBlock(pos);
-    this.detailsPanelState.setFocus(DetailsPanelFocus.PAGE_ITEM);
+    // this.detailsPanelState.setFocus(DetailsPanelFocus.PAGE_ITEM);
   };
 
   private onClick = (e: MouseEvent) => {
