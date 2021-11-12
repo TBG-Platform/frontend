279dd36d1add8912cc2589b4c38702ce8ed8b351
MNewbigging <mrnewbigging@gmail.com>
2021-11-12 20:04:39 +0000

    FRONT-20: more dui panel tests

--- src/editor/dockable-ui/tests/DuiPanel.test.ts
+++ src/editor/dockable-ui/tests/DuiPanel.test.ts
@@ -2,6 +2,27 @@ import { DuiPanel, DuiPanelTab } from '../state/DuiPanel';
 import { DuiPanelModel } from '../model/PanelLayoutModel';
 
 describe('DuiPanel test suite', () => {
+  // Setup a model for the panel
+  const tab1id = 'tab-1';
+  const tab1label = 'Tab 1';
+  const tab1: DuiPanelTab = {
+    id: tab1id,
+    label: tab1label,
+  };
+
+  const tab2id = 'tab-2';
+  const tab2label = 'Tab 2';
+  const tab2: DuiPanelTab = {
+    id: tab2id,
+    label: tab2label,
+  };
+
+  const panelId = 'panel-id';
+  const model: DuiPanelModel = {
+    id: panelId,
+    tabs: [tab1, tab2],
+  };
+
   test('DuiPanel initialises correctly', () => {
     const panelId = 'panel-id';
     const panel = new DuiPanel(panelId);
@@ -13,26 +34,6 @@ describe('DuiPanel test suite', () => {
   });
 
   test('DuiPanel initialises from model', () => {
-    const tab1id = 'tab-1';
-    const tab1label = 'Tab 1';
-    const tab1: DuiPanelTab = {
-      id: tab1id,
-      label: tab1label,
-    };
-
-    const tab2id = 'tab-2';
-    const tab2label = 'Tab 2';
-    const tab2: DuiPanelTab = {
-      id: tab2id,
-      label: tab2label,
-    };
-
-    const panelId = 'panel-id';
-    const model: DuiPanelModel = {
-      id: panelId,
-      tabs: [tab1, tab2],
-    };
-
     const panel = DuiPanel.fromModel(model);
 
     expect(panel).toBeDefined();
@@ -44,5 +45,33 @@ describe('DuiPanel test suite', () => {
     expect(panel.tabs[0].label).toEqual(tab1label);
     expect(panel.tabs[1].id).toEqual(tab2id);
     expect(panel.tabs[1].label).toEqual(tab2label);
+  });
+
+  test('DuiPanel::addTab adds and selects new tab', () => {
+    const tab3id = 'tab-3';
+    const tab3label = 'Tab 3';
+    const tab: DuiPanelTab = {
+      id: tab3id,
+      label: tab3label,
+    };
+
+    const panel = DuiPanel.fromModel(model);
+    panel.addTab(tab);
+
+    expect(panel.tabs).toHaveLength(3);
+    expect(panel.selectedTab).toBeDefined();
+    expect(panel.selectedTab).toEqual(tab);
+  });
+
+  test('DuiPanel::removeTab removes tab and selects another if available', () => {
+    const panel = DuiPanel.fromModel(model);
+
+    expect(panel.selectedTab).toEqual(tab1);
+    expect(panel.tabs).toHaveLength(2);
+
+    panel.removeTab(tab1id);
+
+    expect(panel.selectedTab).toEqual(tab2);
+    expect(panel.tabs).toHaveLength(1);
   });
 });
