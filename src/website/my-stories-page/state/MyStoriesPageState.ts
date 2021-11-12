68fb26d835147b84bca1ca7cba0b63a2ab7adacd
MNewbigging <mrnewbigging@gmail.com>
2021-11-12 17:57:44 +0000

    FRONT-28: my stories page basic layout

--- src/website/my-stories-page/state/MyStoriesPageState.ts
+++ src/website/my-stories-page/state/MyStoriesPageState.ts
@@ -1,1 +1,6 @@
-export class MyStoriesPageState {}
+import { observable } from 'mobx';
+
+export class MyStoriesPageState {
+  // Numbers for now, will use real types later
+  @observable public stories = 0;
+}
